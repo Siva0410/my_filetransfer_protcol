703b10e79c7e556aec6a974e223e5235f4579245
Siva0410 <nmkfngt@s.okayama-u.ac.jp>
2020-11-17 00:27:39 +0900

    ignore check.md5

--- make_randomfile.py
+++ make_randomfile.py
@@ -1,4 +1,14 @@
-f = open('test.txt', 'w')
+#! /usr/bin/env python3
 
-f.write('A'*1024*100) #100kB
-f.close()
+import os, sys, string, random
+
+def get_randomfile(n):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=n))
+
+DATA_PATH = './data'
+os.makedirs(DATA_PATH, exist_ok=True)
+
+for i in range(int(sys.argv[1])):
+    f = open(os.path.join(DATA_PATH, "data"+str(i)),'w')
+    f.write(get_randomfile(102400)) #100kB
+    f.close()
